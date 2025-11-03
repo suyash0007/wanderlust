e94513c22c205edb424ef14c9947b2344d1c22d9
Suyash <tiwarisuyash40@gmail.com>
2025-11-03 14:59:24 +0530

    port change

--- app.js
+++ app.js
@@ -106,6 +106,6 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render("error.ejs", { message });
 })
 
-app.listen(3000, () => {
+app.listen(3002, () => {
     console.log(`App is running smoothly on the port 3000`);
 })
